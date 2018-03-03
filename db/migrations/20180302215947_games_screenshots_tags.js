642032c162f707f990ef1e80335aaf0c83354b49
Beth Johnson <bethjohnson3@gmail.com>
2018-03-02 22:32:48 -0800

    (refactor) modify table creation migration for multiple screenshots and just review summary

--- db/migrations/20180302215947_games_screenshots_tags.js
+++ db/migrations/20180302215947_games_screenshots_tags.js
@@ -1,21 +1,32 @@
+const RATINGS = [
+  'Overwhelmingly Positive',
+  'Very Positive',
+  'Mostly Positive',
+  'Mixed',
+  'Mostly Negative',
+  'Very Negative',
+  'Overwhelmingly Negative',
+];
+
 exports.up = function up(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('games', (table) => {
       table.increments('game_id');
       table.string('title');
+      table.enu('review_rating', RATINGS);
+      table.integer('review_number');
       table.date('release_date');
       table.decimal('price', 5, 2);
       table.string('thumbnail_url');
-      table.string('screen_grab_url');
     }),
     knex.schema.createTable('tags', (table) => {
       table.increments('tag_id');
       table.string('name');
     }),
-    knex.schema.createTable('reviews', (table) => {
-      table.increments('review_id');
+    knex.schema.createTable('screenshots', (table) => {
+      table.increments('id');
       table.integer('game_id');
-      table.integer('score');
+      table.string('screenshot_url');
     }),
     knex.schema.createTable('tags_games', (table) => {
       table.increments('id');
@@ -28,7 +39,7 @@ exports.up = function up(knex, Promise) {
 exports.down = function down(knex, Promise) {
   return Promise.all([
     knex.schema.dropTable('tags_games'),
-    knex.schema.dropTable('reviews'),
+    knex.schema.dropTable('screenshots'),
     knex.schema.dropTable('tags'),
     knex.schema.dropTable('games'),
   ]);
