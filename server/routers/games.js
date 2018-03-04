e22dd4d64fa41ca3c2fdaec83b044ffd36dd1cc9
Beth Johnson <bethjohnson3@gmail.com>
2018-03-03 18:31:14 -0800

    (feat) connects server routes + db

--- server/routers/games.js
+++ server/routers/games.js
@@ -1,13 +1,32 @@
 const express = require('express');
+const games = require('../../db/games');
 
 const router = express.Router();
 
 router
   .get('/:id/related', (req, res) => {
-    res.send();
+    const gameId = Number(req.params.id);
+    games.getRelated(gameId)
+      .then((related) => {
+        res.json(related);
+      })
+      .catch(() => {
+        res.sendStatus(500);
+      });
   })
   .get('/:id', (req, res) => {
-    res.send();
+    const gameId = Number(req.params.id);
+    games.getFullInfo(gameId)
+      .then((game) => {
+        if (!game) {
+          res.sendStatus(400);
+        } else {
+          res.json(game);
+        }
+      })
+      .catch(() => {
+        res.sendStatus(500);
+      });
   });
 
 module.exports = router;
